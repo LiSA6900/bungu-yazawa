13710d7899d3e65525eec8632e15182c8e69f6a1
EC2 Default User <ec2-user@ip-172-31-43-130.ap-northeast-1.compute.internal>
2022-11-15 15:57:31 +0000

    [Add]RSpecのFactoryBotでカタカナのテストデータ取得

--- spec/factories/customer_spec.rb
+++ spec/factories/customer_spec.rb
@@ -1,14 +1,14 @@
-# FactoryBot.define do
-#   factory :customer do
-#     email { Faker::Internet.email}
-#     password { Faker::Lorem.characters(number:6)}
-#     last_name { Faker::Lorem.characters(number:5)}
-#     first_name { Faker::Lorem.characters(number:5)}
-#     last_name_kana { Faker::Lorem.characters(number:5)}
-#     first_name_kana { Faker::Lorem.characters(number:5)}
-#     postal_code { Faker::Lorem.characters(number:7)}
-#     address { Faker::Lorem.characters(number:5)}
-#     telephone_number { Faker::Lorem.characters(number:10)}
-#     is_deleted { Faker::Lorem.characters(number:5)}
-#   end
-# end
+FactoryBot.define do
+  factory :customer do
+    email { Faker::Internet.email}
+    password { ["Abcdef","Skijeh","skdjVaa"].sample}
+    last_name { Faker::Lorem.characters(number:5)}
+    first_name { Faker::Lorem.characters(number:5)}
+    last_name_kana { Gimei.last.katakana }
+    first_name_kana { Gimei.first.katakana}
+    postal_code {  ["1234567","2736582","9635874"].sample}
+    address { Faker::Lorem.characters(number:5)}
+    telephone_number { ["08012345678","0802736582","0809635874"].sample}
+    is_deleted { Faker::Lorem.characters(number:5)}
+  end
+end
