a6fd6e8b76364e42ea412c7aa8d15c6ba1557e5c
EC2 Default User <ec2-user@ip-172-31-43-130.ap-northeast-1.compute.internal>
2022-11-29 10:27:19 +0000

    [Fix]送信先アドレスの隠蔽

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -11,7 +11,7 @@ class InquiryMailer < ApplicationMailer
       @log_in_url = "https://66427f89fd0549c6baf4beba59e779c8.vfs.cloud9.ap-northeast-1.amazonaws.com/admin/sign_in"
     end
     # メール送信先アドレスを設定する
-    mail to: "amnos.1999.arasick@gmail.com",
+    mail to: Rails.application.credentials.gmail[:mail_address],
          subject: "お問い合わせが届きました。" # メール件名
   end
     
