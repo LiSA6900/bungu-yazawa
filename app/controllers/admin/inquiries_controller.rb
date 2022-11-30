0fa2bc636e18cb475af1da09dcfde79e763bcf7b
EC2 Default User <ec2-user@ip-172-31-43-130.ap-northeast-1.compute.internal>
2022-11-30 16:40:30 +0000

    [Fix]管理者トップからページネーション削除

--- app/controllers/admin/inquiries_controller.rb
+++ app/controllers/admin/inquiries_controller.rb
@@ -9,11 +9,11 @@ class Admin::InquiriesController < ApplicationController
     # @inquiries = @inquiries.where('title like ?', "#{params[:search]}").or(@inquiries.where('body like ?', "#{params[:search]}")) if params[:search].present?
     # @inquiries = @inquiries.order(id:"DESC").page(params[:page])
     inquiries = Inquiry.search(params[:search]).order(id:"DESC")
-    @inquiries_unconfirmed = inquiries.unconfirmed.page(params[:page])
-    @inquiries_not_received = inquiries.not_received.page(params[:page])
-    @inquiries_order_received = inquiries.order_received.page(params[:page])
-    @inquiries_done = inquiries.done.page(params[:page])
-    @inquiries = inquiries.page(params[:page])
+    @inquiries_unconfirmed = inquiries.unconfirmed
+    @inquiries_not_received = inquiries.not_received
+    @inquiries_order_received = inquiries.order_received
+    @inquiries_done = inquiries.done
+    @inquiries = inquiries
     @search_word = params[:search]
     render :"admin/homes/top"
   end
