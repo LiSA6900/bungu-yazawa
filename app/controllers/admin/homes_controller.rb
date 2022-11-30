659108d1db0c387e873c2c9c7f9daf9dfcb058fd
EC2 Default User <ec2-user@ip-172-31-43-130.ap-northeast-1.compute.internal>
2022-11-30 16:49:02 +0000

    [Fix]管理者トップの色つくタブ変更

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,10 +3,10 @@ class Admin::HomesController < ApplicationController
   
   def top
     #inquiryコントローラーのsearchメソッドの書き方でもOK
-    @inquiries = Inquiry.order(id:"DESC")
     @inquiries_unconfirmed = Inquiry.where(order_status: "unconfirmed").order(id:"DESC")
     @inquiries_not_received = Inquiry.where(order_status: "not_received").order(id:"DESC")
     @inquiries_order_received = Inquiry.where(order_status: "order_received").order(id:"DESC")
     @inquiries_done = Inquiry.where(order_status: "done").order(id:"DESC")
+    @inquiries = Inquiry.order(id:"DESC")
   end
 end
